7e9d45c6a747058861933332e5b9bb6aff2c8066
Grant Taylor <grantdavidtaylor@outlook.com>
2022-03-14 13:27:00 +0000

    Minor tweaks to pid_check.sh

--- pid_check.sh
+++ pid_check.sh
@@ -4,7 +4,7 @@
 process_id=$1
 scriptname=$2
 
-echo "Checking for any existing process for:" $scriptname
+echo "[$(date)] $scriptname: Checking for any existing processes..."
 
 # Check for any processes matching the script name provided in $1 parameter
 for pid in $(pidof -x $scriptname); do
@@ -13,6 +13,10 @@ for pid in $(pidof -x $scriptname); do
         elapsed_secs=$(ps -q $pid -o etimes=)
         elapsed_time=$(date -d@"$elapsed_secs" -u +%H"h "%M"m "%S"s")
         echo "[$(date)] $scriptname: Process has already been running with PID $pid for $elapsed_time"
+        echo "[$(date)] $scriptname: Exiting."
         exit 1
     fi
 done
+
+echo "[$(date)] $scriptname: No existing processes found, continuing with script."
+
